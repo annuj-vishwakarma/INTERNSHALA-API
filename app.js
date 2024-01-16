edf60cc6f6fc5999a8a52cd0b95839fceeb707e3
anuj <vishwakarmaanuj61@gmail.com>
2024-01-16 16:23:19 +0530

    @

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require("dotenv").config({path:"./env"})
 const express = require("express")
 const app = express();
 
-
+app.get("/", (req ,res,next)=>{
+    res.json({message:"homepage"})
+})
 
 
 app.listen(process.env.PORT, console.log(`server running on port ${process.env.PORT}`))
