7c3631c3d895f0c60bbbc3914fc3fcf2b380f0f0
anuj <vishwakarmaanuj61@gmail.com>
2024-01-19 10:50:34 +0530

    none

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require("express")
 const app = express();
 
 //logger 
-const logger =require("morgan")
+const logger =require("morgan");
+const ErrorHandler = require("./utils/ErrorHandler");
 app.use(logger("tiny"))
 
 //routes
 app.use("/", require("./routes/indexRoutes"))
 
+
+//error handling
+app.all("*",(req,res,next)=>{
+    next(new ErrorHandler(`requested url not found ${req.url}`,404))
+})
+
+app.use(generatedErrors);
 
 
 app.listen(process.env.PORT, console.log(`server running on port ${process.env.PORT}`))
